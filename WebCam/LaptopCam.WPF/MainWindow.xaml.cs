bc06f9f444aaf7caa64e2e908e4fa430cb0e2759
dimaematveev <dimaematveev@gmail.com>
2020-05-31 01:25:34 +0300

    Надо думать

--- WebCam/LaptopCam.WPF/MainWindow.xaml.cs
+++ WebCam/LaptopCam.WPF/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+//Сохраняет каждый десятый кадр
+using System.IO;
+using AForge.Video;//добавить библиотеку
+using AForge.Video.DirectShow;//добавить библиотеку
+using System.Drawing;
 
 namespace LaptopCam.WPF
 {
@@ -23,6 +16,73 @@ namespace LaptopCam.WPF
         public MainWindow()
         {
             InitializeComponent();
+            getCamList();
+            StartFoto();
+
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            CloseVideoSource();
+        }
+
+        private bool DeviceExist = false;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource = null;
+        DirectoryInfo dir;
+        string dirnew;
+        private void StartFoto()
+        {
+            dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            
+            dirnew = dir.CreateSubdirectory("temp").ToString() + "\\";
+            if (DeviceExist)
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                
+                //  CloseVideoSource();
+                videoSource.Start();
+                var z = videoSource.VideoCapabilities;
+            }
+        }
+
+        private void getCamList()
+        {
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+                DeviceExist = true;
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+            }
+        }
+        int i = 0;
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            ++i;
+            if (i % 10 == 0)
+            {
+                FileStream fs = new FileStream(dirnew + i.ToString() + ".Jpeg", FileMode.Create, FileAccess.ReadWrite);
+                Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+                Image im = img;
+                img.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            }
+        }
+        private void CloseVideoSource()
+        {
+            if (!(videoSource == null))
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    videoSource = null;
+                }
         }
     }
 }
