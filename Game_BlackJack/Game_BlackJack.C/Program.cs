6a5036550acd2230125a93a7a3790e86692d551a
dimaematveev <dimaematveev@gmail.com>
2020-07-09 20:18:53 +0300

    Игра БлэкДжэк

---
+++ Game_BlackJack/Game_BlackJack.C/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_BlackJack.C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
